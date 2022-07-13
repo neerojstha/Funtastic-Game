8cf6f867c932fcbcdce4f0131ee28086353eb3da
neerojstha <nirojstha@hotmail.com>
2022-07-13 03:40:08 +0000

    js respose updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,8 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 function runGame(gameType) {
 
+    document.getElementById("answer-box").value = "";
+
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
